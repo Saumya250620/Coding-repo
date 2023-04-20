82471a40e8a857451bc606ed995b614c93abe2d2
Saumya250620 <78815522+Saumya250620@users.noreply.github.com>
2023-04-21 03:14:53 +0530

    Add files via upload

---
+++ C_C++/map_STL4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <map>
+using namespace std;
+
+int main(){
+    map<int, string> m;
+    m.insert({5, "gfg"});
+    m.insert({2, "ide"});
+    m.insert({1, "practice"});
+
+    auto it = m.upper_bound(2);
+    if(it!= m.end())
+        cout << (*it).first << endl;
+    else
+        cout << "Greater Value";
+    m.erase(5);
+    cout << "Size: " << m.size() << endl;
+    
+    m.erase(m.find(2), m.end());
+    cout << "Size: " << m.size() << endl;
+
+    return 0;
+}
